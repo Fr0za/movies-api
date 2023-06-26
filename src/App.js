fb1d004df9b846df2783b703ae81223a6218dc43
Fr0za <jonas.zukauskass@gmail.com>
2023-06-26 21:43:20 +0300

    Installed packages, and created blank pages of project.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+
+import Card from './Components/Card/Card';
+import PageHeader from './Components/PageHeader/PageHeader';
+import PageFooter from './Components/PageFooter/PageFooter';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+
+import Home from './Pages/Home/Home';
+import Authors from './Pages/Authors/Authors';
+import Author from './Pages/Author/Author';
+import AuthorForm from './Pages/AuthorForm/AuthorForm';
+import Movies from './Pages/Movies/Movies';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Container>
+        <PageHeader />
+      </Container>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/authors' element={<Authors />} />
+        <Route path='/authors/:id' element={<Author />} />
+        <Route path='/authorform' element={<AuthorForm />} />
+        <Route path='/authorform/:id' element={<AuthorForm />} />
+        <Route path='/movies' element={<Movies />} />
+
+
+        <Route path='*' element={
+          <Container>
+            <Card>
+              <h1>Page not found</h1>
+              <Link to='/'>Go Back to home page</Link>
+            </Card>
+          </Container>
+        } />
+      </Routes>
+
+      <ToastContainer 
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="colored"
+      />
+
+      <Container>
+        <PageFooter />
+      </Container>
+
     </div>
   );
 }
